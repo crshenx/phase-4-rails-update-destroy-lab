cd4bc77952ae76ac7b04574980451db766a32801
Chris Hendricks <crshenx@gmail.com>
2022-08-10 13:33:13 -0700

    commit tree e69aacf6364246d4fb49977e601f12aaa1717ca5

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,23 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(is_in_stock: false)
+    render json: plant
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    # if plant
+      plant.destroy
+      head :no_content
+    # else
+    #   render json: { error: "Plant not found" }, status: :not_found
+    # end
+  
+  end
+
   private
 
   def plant_params
